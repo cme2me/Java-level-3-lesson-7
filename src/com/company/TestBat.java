6fa4eb684bb7bb7ca2295a394aeb8a5c686ff969
cme2me <max.zhakov@gmail.com>
2022-04-11 18:36:22 +0300

    Реализовал AfterSuite, BeforeSuite, приоритет тестов и сам запускатор

--- src/com/company/TestBat.java
+++ src/com/company/TestBat.java
@@ -1,4 +1,48 @@
 package com.company;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class TestBat {
+    public static void start(Class c) {
+        List<Method> methods = new ArrayList<>();
+        Method[] classMethods = c.getDeclaredMethods();
+
+        for (Method method : classMethods) {
+            if (method.isAnnotationPresent(TestPriority.class)) {
+                methods.add(method);
+            }
+        }
+        methods.sort(Comparator.comparingInt((Method i) -> i.getAnnotation(TestPriority.class).priority()).reversed());
+        for (Method method : classMethods) {
+            if (method.isAnnotationPresent(BeforeSuite.class)) {
+                if (methods.size() > 0 && methods.get(0).isAnnotationPresent(BeforeSuite.class)) {
+                    throw new RuntimeException("@BeforeSuite annotation method > 1");
+                }
+                methods.add(0, method);
+            }
+        }
+
+        for (Method method : classMethods) {
+            if (method.isAnnotationPresent(AfterSuite.class)) {
+                if (methods.size() > 0 && methods.get(methods.size() - 1).isAnnotationPresent(AfterSuite.class)) {
+                    throw new RuntimeException("@AfterSuite annotation method > 1");
+                }
+                methods.add(method);
+            }
+        }
+
+        for (Method method : methods) {
+            try {
+                method.invoke(null);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
